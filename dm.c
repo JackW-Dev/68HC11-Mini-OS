30bbfdfea2ba2998d647b4007ee29e1ebfa5a698
Jack Walker <74609541+notpengy@users.noreply.github.com>
2021-03-15 13:15:50 +0000

    Display Memory v1.0
    Fully operational display memory function that replicates the style of the built in version provided in the on board EPROM.

--- dm.c
+++ dm.c
@@ -2,15 +2,6 @@
 #include <string.h>
 
 void main() {
-	unsigned int startAddress = 0;
-	
-	printf("Start address: ");
-	scanf("%x", &startAddress);
-	
-	displayMemory(startAddress);
-}
-
-int displayMemory(unsigned int startAddress) {
 	/*
 	Function: Display Memory
 	Operation: Allow the user to view the values stored in memory
@@ -18,14 +9,47 @@ int displayMemory(unsigned int startAddress) {
 	Date: 20/02/2021
 	Version: 1.0
 	Change log:
+	v1.0 - Output values correctly - Some strange characters appearing
+	v1.1 - Fix for valid ASCII codes being between 0x20 and 0x7f
 	Produced by: Jack Walker
 	*/
 	
-	/*Step 1 - Ask for start address*/
-	/*Step 2 - Validate user input and loop if invalid*/
-	/*Step 3 - Display contents of memory from the starting address to the address + 100 (if possible)*/
+	unsigned char stringInp[10];
+	unsigned char *startAddress;
+	int i, j, k;
+	unsigned char data;
 	
-	printf("Start address: %x\n\r", startAddress);	
 	printf("Display Memory\n\r");
+	
+	printf("Start address: ");
+	gets(stringInp);
+	sscanf(stringInp, "%x", &startAddress);	
+	printf("Start address: %x\n\r", startAddress);
+	
+	printf("Address\t\t Hex\t\t\t ASCII\n\r");
+	
+	/*Loop through 16 times, incrementing by 10 each time to pring the 16 lines*/
+	for (i = 0; i < 160; i = (i + 10)) {		
+		printf(" %04x\t ", startAddress);
+		/*Loop through 10 values from startAddress and output the hex stored in the address*/
+		for (j = 0; j < 10; j++) {
+			data = *(startAddress + j);			
+			printf("%02x ", data);			
+		}
+		printf("\t");
+		/*Loop through 10 values from startAddress and output the cahr stored in the address
+		Output "." if the value is not in the ASCII table*/
+		for (k = 0; k < 10; k++) {
+			data = *(startAddress + k);
+			/*Hex values between 0x20 and 0x7f are valid, printable characters.
+			Those before are special characters such as SPACE and NUL*/
+			if (data > 0x20 && data < 0x7f) {
+				printf("%c", data);
+			} else {
+				printf(".", data);
+			}
+		}
+		printf("\n\r");
+		startAddress = startAddress + 10;
+	}
 }
-
