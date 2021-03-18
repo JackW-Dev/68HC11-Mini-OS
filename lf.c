65957d2c414b2e76ed14a6942213dc117b100f63
Jack Walker <74609541+notpengy@users.noreply.github.com>
2021-03-18 14:05:51 +0000

    Updated to read length and address
    TODO: Fix reading for data and checksum
    Handle memory storage and verification

--- lf.c
+++ lf.c
@@ -13,6 +13,7 @@ int loadFile() {
 	Date: 20/02/2021
 	Version: 1.0
 	Change log:
+	v1.0 - Implemented basic file load that will read in the length of the file
 	Produced by: Jack Walker
 	*/
 	
@@ -21,63 +22,87 @@ int loadFile() {
 	/*Step 3 - Output some form of progress indicator*/
 	/*Step 4 - Output when file fully read*/	
 		
-	unsigned char *scsr, *scdr, data, length, status, charcount, flag;
-	unsigned char inputData[73];
-	unsigned int len, origin, checksum, i;
+	unsigned char *scsr, *scdr, data, status, charcount, flag;
+	/*Pre-initialised due to strange issue when outputting values using printf*/
+	unsigned char lenArr[3] = {'0', '0', '\0'}, originArr[5] = {'0', '0', '0', '0', '\0'}, dataArr[65], checksumArr[3] = {'0', '0', '\0'};
+	unsigned int lineLen, origin, checksum, i;
 	int linecount;
 	
-	charcount = 0;
-	linecount = 0;
-	flag = 0;
-	status = 0;
+	charcount = 0, linecount = 0, flag = 0,	status = 0;
+	dataArr[64] = '\0';
 
 	scsr = (unsigned char *) 0x2e;
 	scdr = (unsigned char *) 0x2f;
+	
 	do {
 		while(((*scsr) & 0x20) == 0x00);
 		
 		data = *scdr;
 		
 		if (data == 'S') {
-			flag = 0; /* reset char count */	
+			flag = 0; /*Reset char count */	
 			
 			while(((*scsr) & 0x20) == 0x00);
 			
 			data = *scdr;
 			if (data == '1') {
 				charcount = 0;				
-				putchar('~'); /*optional to save time remove */				
-				flag = 1;  /* tell to start counting characters */				
+				putchar('~'); /*Optional to save time remove*/				
+				flag = 1;  /*Tell to start counting characters*/				
 				linecount++;
 			}
 			if (data == '9') {
-				status = 1; /* Temp stop if S9 detected */
+				status = 1; /*Temp stop if S9 detected*/
 			}
 		}
-		if (flag == 1) {
+		
+		if (flag == 1) {						
 			/*Extract length of line*/	
 			while(((*scsr) & 0x20) == 0x00);			
-			inputData[0] = *scdr;
+			lenArr[0] = *scdr;
 			
 			while(((*scsr) & 0x20) == 0x00);
-			inputData[1] = *scdr;
+			lenArr[1] = *scdr;
+						
+			/*Extract start address for line*/
+			for (i = 0; i < 4; i++) {
+				while(((*scsr) & 0x20) == 0x00);			
+				originArr[i] = *scdr;
+			}		
+						
+			/*TODO: Loop through reading each byte and appending to array*/		
+			for (i = 0; i < lineLen - 6; i++) {
+				while(((*scsr) & 0x20) == 0x00);			
+				dataArr[i] = *scdr;
+			}
+									
+			while(((*scsr) & 0x20) == 0x00);			
+			checksumArr[0] = *scdr;
 			
-			sscanf(inputData, "%x", &len);
+			while(((*scsr) & 0x20) == 0x00);
+			checksumArr[1] = *scdr;
 			
-			printf("%x\n\r", len);
+			sscanf(lenArr, "%x", &lineLen);			
+			printf("Line len: %x\n\r", lineLen);
 			
-			/*TODO: Loop through reading each byte and appending to array*/
+			sscanf(originArr, "%x", &origin);			
+			printf("Origin: %x\n\r", origin);
 			
-			flag = 0;			
-	
-			/* do rest extract length, address*/
-			/* You can use XON/XOFF or change delay on hyperterm */
-			/*Don't forget to ignore all characters after line length */
-		}
-		
+			printf("Data: %s\n\r", dataArr);
+			
+			sscanf(checksumArr, "%x", &checksum);			
+			printf("Checksum: %x\n\r", checksum);
+									
+			flag = 0;
+				
+			/*Do rest extract length, address*/
+			/*You can use XON/XOFF or change delay on hyperterm*/
+			/*Don't forget to ignore all characters after line length*/
+		}	
+			
 	} while (status == 0);
-		
-	printf("\n\rDownload completed S %d lines \n\r", linecount);
-	printf("Last S1 Address + data + checksum = %02x \n\r", ((charcount-1)/2) - 1);
+	
+	printf("\n\rDownload completed S %d lines\n\r", linecount);
+	printf("Last S1 Address + data + checksum = %02x\n\r", ((charcount - 1) / 2) - 1);
 	return(0);
 }
