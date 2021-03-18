0b6d098472987b7fc3110d9f85061c06aaa0ea35
Jack Walker <74609541+notpengy@users.noreply.github.com>
2021-03-18 23:52:36 +0000

    Implemented modification and some validation
    Implemented memory modification with ability to skip and stop.
    Implemented ability to change value in memory to a new hex value.
    TODO: Validate to only accept valid hex values.

--- mm.c
+++ mm.c
@@ -2,15 +2,6 @@
 #include <string.h>
 
 void main() {
-	unsigned int startAddress = 0;
-	
-	printf("Start address: ");
-	scanf("%x", &startAddress);
-	
-	modifyMemory(startAddress);
-}
-
-int modifyMemory(unsigned int startAddress) {
 	/*
 	Function: Modify Memory
 	Operation: Allow the user to modify values stored in memory
@@ -18,6 +9,8 @@ int modifyMemory(unsigned int startAddress) {
 	Date: 20/02/2021
 	Version: 1.0
 	Change log:
+	v1.0 - Will iterate over addresses and ask for input. Error - All inputs are being written as 72
+	v1.1 - Iterates over addresses and allows the user to update value. TODO: Validate input.
 	Produced by: Jack Walker
 	*/
 	
@@ -26,7 +19,43 @@ int modifyMemory(unsigned int startAddress) {
 	/*Step 3 - Validate that the data is storable and store (loop if can't store)*/
 	/*Step 4 - Increment to next address and repeat process*/
 	/*Step 5 - Terminate if '.' is input*/
+
+	unsigned char stringInp[5], dataInp[3];
+	unsigned char *startAddress;
+	unsigned char data, topNibble, bottomNibble;
+	unsigned int hexData;
+	int status = 0, i;
 	
-	printf("Start address: %x\n\r", startAddress);	
 	printf("Modify Memory\n\r");
+	
+	printf("Start address: ");
+	gets(stringInp);
+	sscanf(stringInp, "%x", &startAddress);	
+	
+	do {
+		printf("%04x\t%02x : ", startAddress, *startAddress);
+		gets(dataInp);
+		
+		sscanf(dataInp, "%c", &data);
+		printf("%c\n\r", data);
+		
+		if (data == '.') {
+			status = 1;
+		}
+		if (data == '\n') {
+			startAddress++;
+		}
+		
+		sscanf(dataInp, "%02x", &hexData);
+		printf("%02x\n\r", hexData);
+		
+		if (hexData <= 255) {
+			*startAddress = hexData;
+			startAddress++;
+		} else {
+			prinf("Invalid input."); 
+			printf("Please use . to stop, carriage return to skip or Hex to input.\n\r");
+		}			
+	} while (status == 0);
 }
+
