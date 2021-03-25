c3a044c659fe7b04278797f5b3efb7cc7fdc581c
Jack Walker <74609541+notpengy@users.noreply.github.com>
2021-03-25 17:55:38 +0000

    Memory Modify v1.3
    Fully operational modify memory function that replicates the style of the built in version provided in the on board EPROM.

--- mm.c
+++ mm.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+void modifyMemory(unsigned int);
+
 void main() {
+	unsigned int start = 0x4600;
+
+	modifyMemory(start);
+}
+
+void modifyMemory(unsigned int start) {
 	/*
 	Function: Modify Memory
 	Operation: Allow the user to modify values stored in memory
 	Returns: Integer error code
-	Date: 20/02/2021
-	Version: 1.0
+	Date: 25/03/2021
+	Version: 1.3
 	Change log:
 	v1.0 - Will iterate over addresses and ask for input. Error - All inputs are being written as 72.
 	v1.1 - Iterates over addresses and allows the user to update value. TODO: Validate input.
@@ -16,23 +24,25 @@ void main() {
 	Produced by: Jack Walker
 	*/
 	
-	unsigned char stringInp[10], dataInp[10];
+	unsigned char /*stringInp[10],*/ dataInp[10];
 	unsigned char *startAddress;
-	unsigned char data, skipFlag, check, check2, check3;
+	unsigned char data, skipFlag, check, check2, check3, check4;
 	unsigned int hexData;
 	int status, i;
 	
 	status = 0;
 	
-	printf("Modify Memory\n\r");
+	/*printf("Modify Memory\n\r");
 	
 	printf("Start address: ");
 	gets(stringInp);
-	sscanf(stringInp, "%x", &startAddress);	
+	sscanf(stringInp, "%x", &startAddress);*/
+	
+	startAddress = (unsigned char *)start;
 	
 	do {
-		/*Reset the dataInp array, data and skipFlag to reduce risk of remaining data causing errors*/
-		
+			
+		/*Reset the dataInp array, data and skipFlag to reduce risk of remaining data causing errors*/		
 		dataInp[0] = '\n';
 		dataInp[1] = '\0';
 		data = '\0';
@@ -43,34 +53,32 @@ void main() {
 		
 		sscanf(dataInp, "%c", &data);
 				
-		/*If the input is a full stop, set the status flag to stop MM and set skipFlag to prevent modify*/
-		
+		/*If the input is a full stop, set the status flag to stop MM and set skipFlag to prevent modify*/		
 		if (data == '.') {
 			status = 1;
 			skipFlag = 1;
 		}
 		
-		/*If the input is a new line, increment the start address and set skipFlag to prevent modify*/
-		
+		/*If the input is a new line, increment the start address and set skipFlag to prevent modify*/		
 		if (data == '\n') {
 			startAddress++;
 			skipFlag = 1;
 		}
 		
-		/*If skipFlag is not set, validate the input and either modify memory or report an error*/
-		
+		/*If skipFlag is not set, validate the input and either modify memory or report an error*/		
 		if (skipFlag == 0) {
 								
 			/*Check if valid hex input:
 			- Both dataInp[0] and dataInp[1] are hex and dataInp[2] is '\0'
 			- dataInp[0] is hex and dataInp[1] is '\0'
-			- dataInp[0] is ' ', dataInp[1] is hex and dataInp[2] is '\0'*/		
-				
+			- dataInp[0] is ' ', dataInp[1] is hex and dataInp[2] is '\0'
+			- dataInp[0] is hex, dataInp[1] is ' ' and dataInp[2] is '\0'*/						
 			check = (isxdigit(dataInp[0]) && isxdigit(dataInp[1]) && dataInp[2] == '\0');
 			check2 = (isxdigit(dataInp[0]) && dataInp[1] == '\0');
 			check3 = (dataInp[0] == ' ' && isxdigit(dataInp[1]) && dataInp[2] == '\0');
+			check4 = (isxdigit(dataInp[0]) && dataInp[1] == ' ' && dataInp[2] == '\0');
 					
-			if (check || check2 || check3) {
+			if (check || check2 || check3 || check4) {
 				sscanf(dataInp, "%02x", &hexData);
 				*startAddress = hexData;
 				startAddress++;
