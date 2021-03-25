bb8892631e118a74771bf84d4a9f33cd3cdfed75
Jack Walker <74609541+notpengy@users.noreply.github.com>
2021-03-25 17:51:44 +0000

    Demo v1.1
    Fully operational demo function that replicates the style of the built in version provided in the on board EPROM.
    This function reads in the value of a potentiometer on port E0 and adjusts the speed of values ouput from ports A0 to A4 to change the rotational speed of a servo.

--- demo.c
+++ demo.c
@@ -1,96 +1,87 @@
 #include <stdio.h>
 
+void demo();
+
 void main() {
+	demo();
+}
+
+void demo() {	
 	/*
 	Function: Demo
 	Operation: Demo program using a variable resistor to step a motor
 	Returns: Integer error code
-	Date: 20/02/2021
-	Version: 1.0
+	Date: 25/03/2021
+	Version: 1.1
 	Change log:
+	v1.0 - Read value on port E0 and produce an offset to delay the output on ports A0 - A3
+	v1.1 - Changed offset from multiplication to addition to allow more granular control (smaller offsets for higher speed)
 	Produced by: Jack Walker
 	*/
-	
-	/*Step 1 - Start demo program using ports*/
-	/*Step 2 - Set up E0 to continuous read mode to read resistance value*/
-	/*Step 3 - Set up porta A0-A3 as output and set a looping output sequence*/
-	/*Step 4 - Read in val on E0 and set as time delay for outputs on A0-A3*/
 	
 	unsigned char *adControlStatus, *adResults1, *portA, *ddrA, portAMask, data;
 	unsigned int i, delay;
 	
 	adControlStatus = (unsigned char*)0x30;
 	adResults1 = (unsigned char*)0x31;
-	portA = (unsigned char *)0x00; /*Cast to pointers*/
+	
+	/*Cast to pointers*/
+	portA = (unsigned char *)0x00; 
 	ddrA = (unsigned char *)0x01;
 	*ddrA = 0xff;	
 	
-	printf("Demo\n\r");	
+	/*printf("Demo\n\r");*/
 	
 	*adControlStatus =  0x20; /*Single continuous scan mode*/
 	
 	for(;;) {
-		while(((*adControlStatus) & 0x80) == 0x00); /*Wait for E0C*/
-		printf("Data from E0 is %02x\n\r", *adResults1);
-		delay = *adResults1 * 0xff;	/*Multiply by 0xff to create delay*/
-		printf("Delay is %04x\n\r", delay);
-		for (i = 0; i < delay; i++);	/*Delay based on resistor value read on E0*/
+		/*Wait for E0C*/
+		while(((*adControlStatus) & 0x80) == 0x00); 
+		
+		/*printf("Data from E0 is %02x\n\r", *adResults1);*/
+		
+		/*Add 0xff to create delay*/
+		delay = (*adResults1) + 0xff;
+			
+		/*printf("Delay is %04x\n\r", delay);*/
+		
+		/*Delay based on resistor value read on E0*/
+		for (i = 0; i < delay; i++);	
 		
 		*portA = 0x01;
-		
-		while(((*adControlStatus) & 0x80) == 0x00); /*Wait for E0C*/
-		printf("Data from E0 is %02x\n\r", *adResults1);
-		delay = *adResults1 * 0xff;	/*Multiply by 0xff to create delay*/
-		printf("Delay is %04x\n\r", delay);
-		for (i = 0; i < delay; i++);	/*Delay based on resistor value read on E0*/		
-		
+
+		/*Delay based on resistor value read on E0*/
+		for (i = 0; i < delay; i++);	
+			
 		*portA = 0x03;
-		
-		while(((*adControlStatus) & 0x80) == 0x00); /*Wait for E0C*/
-		printf("Data from E0 is %02x\n\r", *adResults1);
-		delay = *adResults1 * 0xff;	/*Multiply by 0xff to create delay*/
-		printf("Delay is %04x\n\r", delay);
-		for (i = 0; i < delay; i++);	/*Delay based on resistor value read on E0*/
+
+		/*Delay based on resistor value read on E0*/
+		for (i = 0; i < delay; i++);
 		
 		*portA = 0x02;
 				
-		while(((*adControlStatus) & 0x80) == 0x00); /*Wait for E0C*/
-		printf("Data from E0 is %02x\n\r", *adResults1);
-		delay = *adResults1 * 0xff;	/*Multiply by 0xff to create delay*/
-		printf("Delay is %04x\n\r", delay);
-		for (i = 0; i < delay; i++);	/*Delay based on resistor value read on E0*/
+		/*Delay based on resistor value read on E0*/
+		for (i = 0; i < delay; i++);
 		
 		*portA = 0x06;
-		
-		while(((*adControlStatus) & 0x80) == 0x00); /*Wait for E0C*/
-		printf("Data from E0 is %02x\n\r", *adResults1);
-		delay = *adResults1 * 0xff;	/*Multiply by 0xff to create delay*/
-		printf("Delay is %04x\n\r", delay);
-		for (i = 0; i < delay; i++);	/*Delay based on resistor value read on E0*/		
+	
+		/*Delay based on resistor value read on E0*/
+		for (i = 0; i < delay; i++);	
 		
 		*portA = 0x04;
 		
-		while(((*adControlStatus) & 0x80) == 0x00); /*Wait for E0C*/
-		printf("Data from E0 is %02x\n\r", *adResults1);
-		delay = *adResults1 * 0xff;	/*Multiply by 0xff to create delay*/
-		printf("Delay is %04x\n\r", delay);
-		for (i = 0; i < delay; i++);	/*Delay based on resistor value read on E0*/		
+		/*Delay based on resistor value read on E0*/
+		for (i = 0; i < delay; i++);		
 		
 		*portA = 0x0c;
 		
-		while(((*adControlStatus) & 0x80) == 0x00); /*Wait for E0C*/
-		printf("Data from E0 is %02x\n\r", *adResults1);
-		delay = *adResults1 * 0xff;	/*Multiply by 0xff to create delay*/
-		printf("Delay is %04x\n\r", delay);
-		for (i = 0; i < delay; i++);	/*Delay based on resistor value read on E0*/	
+		/*Delay based on resistor value read on E0*/
+		for (i = 0; i < delay; i++);	
 		
 		data = 0x08;
 				
-		while(((*adControlStatus) & 0x80) == 0x00); /*Wait for E0C*/
-		printf("Data from E0 is %02x\n\r", *adResults1);
-		delay = *adResults1 * 0xff;	/*Multiply by 0xff to create delay*/
-		printf("Delay is %04x\n\r", delay);
-		for (i = 0; i < delay; i++);	/*Delay based on resistor value read on E0*/	
+		/*Delay based on resistor value read on E0*/
+		for (i = 0; i < delay; i++);	
 		
 		*portA = 0x09;
 	}
